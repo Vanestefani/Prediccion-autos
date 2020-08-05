0f0bb08509cd34218333b00b0788444af51dbb67
vanessa <vanessa.estefania.corredor@gmail.com>
2020-08-05 17:19:45 -0500

    definir modelo

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-console.log('Hello TensorFlow');
+console.log("Hello TensorFlow");
 /**
  * Get the car data reduced to just the variables we are interested
  * and cleaned of missing data.
  */
 async function getData() {
-    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
-    const carsData = await carsDataReq.json();
-    const cleaned = carsData.map(car => ({
+  const carsDataReq = await fetch(
+    "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
+  );
+  const carsData = await carsDataReq.json();
+  const cleaned = carsData
+    .map((car) => ({
       mpg: car.Miles_per_Gallon,
       horsepower: car.Horsepower,
     }))
-    .filter(car => (car.mpg != null && car.horsepower != null));
+    .filter((car) => car.mpg != null && car.horsepower != null);
 
-    return cleaned;
-  }
+  return cleaned;
+}
 
-  async function run() {
-    // Load and plot the original input data that we are going to train on.
-    const data = await getData();
-    const values = data.map(d => ({
-      x: d.horsepower,
-      y: d.mpg,
-    }));
-// graficar
-    tfvis.render.scatterplot(
-      {name: 'Horsepower v MPG'},
-      {values},
-      {
-        xLabel: 'Horsepower',
-        yLabel: 'MPG',
-        height: 300
-      }
-    );
+async function run() {
+  // Load and plot the original input data that we are going to train on.
+  const data = await getData();
+  const values = data.map((d) => ({
+    x: d.horsepower,
+    y: d.mpg,
+  }));
+  // graficar
+  tfvis.render.scatterplot(
+    { name: "Horsepower v MPG" },
+    { values },
+    {
+      xLabel: "Horsepower",
+      yLabel: "MPG",
+      height: 300,
+    }
+  );
+}
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
 
-  
-  }
+  // Add a single input layer
+  model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
 
-  document.addEventListener('DOMContentLoaded', run);
+  // Add an output layer
+  model.add(tf.layers.dense({ units: 1, useBias: true }));
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+  return model;
+}
+
+document.addEventListener("DOMContentLoaded", run);
